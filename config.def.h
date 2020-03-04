3f43c30cb32601f61a6cb26394ef249f71ff9ddd
Steven Hall <Hallzy.18@gmail.com>
2020-03-03 20:15:54 -0800

    Open URL Patch

--- config.def.h
+++ config.def.h
@@ -186,6 +186,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ MODKEY,               XK_o,           opencopied,     {.v = "xdg-open"} },
 };
 
 /*
